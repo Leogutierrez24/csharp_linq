a6a390dd48f48964c98cff30f51ff56abce860c3
Leogutierrez24 <leoalegutierrez97@gmail.com>
2023-08-11 18:35:10 -0300

    finished the course. Added a more methods in query class about new structures from linq. Need to add a readme to describe the info in this repo

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,13 +1,17 @@
+using System.IO.Compression;
+
 class LinqQueries
 {
     private List<Book> _booksCollection = new List<Book>();
 
-    public LinqQueries() 
+    public LinqQueries()
     {
-        using(StreamReader sr = new StreamReader("books.json"))
+        using (StreamReader sr = new StreamReader("books.json"))
         {
             string json = sr.ReadToEnd();
-            this._booksCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true})!;
+            this._booksCollection =
+            System.Text
+            .Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true })!;
         }
     }
 
@@ -27,9 +31,9 @@ class LinqQueries
 
     public IEnumerable<Book> GetBooksByPagesAndTitle(int pages, string title)
     {
-        return from book in _booksCollection 
-        where book.PageCount >= pages && book.Title.Contains(title)
-        select book;
+        return from book in _booksCollection
+               where book.PageCount >= pages && book.Title.Contains(title)
+               select book;
     }
 
     public bool AllBooksHaveStatus()
@@ -45,23 +49,117 @@ class LinqQueries
     public IEnumerable<Book> GetPythonBooks()
     {
         return from book in this._booksCollection
-        where book.Categories.Contains("Python")
-        select book;
+               where book.Categories.Contains("Python")
+               select book;
     }
 
     public IEnumerable<Book> GetJavaBooksOrder()
     {
         return from book in this._booksCollection
-        where book.Categories.Contains("Java")
-        orderby book.Title
-        select book;
+               where book.Categories.Contains("Java")
+               orderby book.Title
+               select book;
     }
 
     public IEnumerable<Book> GetBooksByPagesDescending(int pages)
     {
         return from book in this._booksCollection
-        where book.PageCount > pages
-        orderby book.PageCount descending
-        select book;
+               where book.PageCount > pages
+               orderby book.PageCount descending
+               select book;
+    }
+
+    public IEnumerable<Book> GetBooksByFilter(int booksQty, string category)
+    {
+        return this._booksCollection.Where(book => book.Categories.Contains(category))
+        .OrderByDescending(book => book.PublishedDate)
+        .Take(booksQty);
+    }
+
+    public IEnumerable<Book> UsingSkipQuery(int pagesQty) // I don't know how to name this method
+    {
+        return this._booksCollection.Where(book => book.PageCount > pagesQty)
+        .Take(4)
+        .Skip(2);
+    }
+
+    public IEnumerable<Book> GetBookTitleandPageCount()
+    {
+        return _booksCollection.Take(3)
+        .Select(book => new Book() { Title = book.Title, PageCount = book.PageCount });
+    }
+
+    public int GetBooksQtyByPageCount(int minValue, int maxValue)
+    {
+        return _booksCollection.Count(book => book.PageCount >= minValue && book.PageCount <= maxValue);
+    }
+
+    public DateTime GetFirstBookPublished()
+    {
+        return _booksCollection.Min(book => book.PublishedDate);
+    }
+
+    public int GetBookWithMostPagesCount()
+    {
+        return _booksCollection.Max(book => book.PageCount);
+    }
+
+    public Book GetBookWithFewestPageCount()
+    {
+        return _booksCollection.Where(book => book.PageCount > 0)
+        .MinBy(book => book.PageCount)!;
+    }
+
+    public Book GetNewestBook()
+    {
+        return _booksCollection.MaxBy(book => book.PublishedDate)!;
+    }
+
+    public int SumOfAllPageCount(int min, int max)
+    {
+        return _booksCollection.Where(book => book.PageCount >= min && book.PageCount <= max).Sum(book => book.PageCount);
+    }
+
+    public string BooksBeforeYear(int year)
+    {
+        return _booksCollection.Where(book => book.PublishedDate.Year > year)
+        .Aggregate("", (bookTitle, next) =>
+        {
+            if (bookTitle != string.Empty)
+            {
+                bookTitle += Environment.NewLine + next.Title;
+            }
+            else
+            {
+                bookTitle += next.Title;
+            }
+
+            return bookTitle;
+        });
+    }
+
+    public double TitlesCharAverage()
+    {
+        return _booksCollection.Average(book => book.Title.Length);
+    }
+
+    public IEnumerable<IGrouping<int, Book>> GetBooksBeforeYear(int year)
+    {
+        return _booksCollection.Where(book => book.PublishedDate.Year > year)
+        .GroupBy(book => book.PublishedDate.Year);
+    }
+
+    public ILookup<char, Book> BooksDictionary()
+    {
+        return _booksCollection.ToLookup(book => book.Title[0], book => book);
+    }
+
+    public IEnumerable<Book> UsingJoinLinq()
+    {
+        IEnumerable<Book> booksAfter2005 = _booksCollection.Where(book => book.PublishedDate.Year > 2005);
+
+        IEnumerable<Book> booksWith500Pages = _booksCollection.Where(book => book.PageCount > 500);
+
+        return booksAfter2005.Join(booksWith500Pages, firstCollection => firstCollection.Title, secondCollection => secondCollection.Title, (firstCollection, secondCollection) => firstCollection);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
 ﻿static void PrintValues(IEnumerable<Book> bookList)
 {
     Console.WriteLine("{0, -70} {1, 7} {2, 11}\n", "Title", "Pages", "Published Date");
-    foreach(Book item in bookList)
+    foreach (Book item in bookList)
     {
         Console.WriteLine("{0, -70} {1, 7} {2, 11}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
+
+static void PrintGroups(IEnumerable<IGrouping<int, Book>> booksList)
+{
+    foreach(var group in booksList)
+    {
+        Console.WriteLine($"\nGroup: {group.Key}");
+        Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", "Title", "Pages", "Published Date");
+        foreach(var item in group)
+        {
+            Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+        }
+    }
+}
+
+static void PrintDictionary(ILookup<char, Book> booksList, char someChar)
+{
+    Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", "Title", "Pages", "Published Date");
+    foreach(var item in booksList[someChar])
+    {
+        Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
     }
 }
 
@@ -18,11 +40,55 @@ LinqQueries queries = new();
 // Getting by pages and title filter
 PrintValues(queries.GetBooksByPagesAndTitle(250, "in Action"));
 
-Console.WriteLine($"All the books have status: {queries.AllBooksHaveStatus()}");
-Console.WriteLine($"Is there any book published on 2005?: {queries.BookPublishedOnYear(2005)}");
+// Console.WriteLine($"All the books have status: {queries.AllBooksHaveStatus()}");
+// Console.WriteLine($"Is there any book published on 2005?: {queries.BookPublishedOnYear(2005)}");
 
-PrintValues(queries.GetPythonBooks());
+// PrintValues(queries.GetPythonBooks());
 
-PrintValues(queries.GetJavaBooksOrder());
+// OrderBy
+// PrintValues(queries.GetJavaBooksOrder());
 
-PrintValues(queries.GetBooksByPagesDescending(450));
+// OrderByDescending
+// PrintValues(queries.GetBooksByPagesDescending(450));
+
+// Take
+// PrintValues(queries.GetBooksByFilter(3, "Java"));
+
+// Skip
+//PrintValues(queries.UsingSkipQuery(400));
+
+// Select
+// PrintValues(queries.GetBookTitleandPageCount());
+
+// Count and LongCount
+Console.WriteLine($"The qty of books with 200 and 500 pages are: {queries.GetBooksQtyByPageCount(200, 500)}");
+
+// Min and Max
+Console.WriteLine($"The first book published was in {queries.GetFirstBookPublished().ToShortDateString()}");
+
+Console.WriteLine($"The book with most page count have: {queries.GetBookWithMostPagesCount()} pages");
+
+// MinBy and MaxBy
+Book bookWithFewestPages = queries.GetBookWithFewestPageCount();
+Console.WriteLine($"The book with fewest page count is {bookWithFewestPages.Title} with {bookWithFewestPages.PageCount} pages.");
+
+Book newestBook = queries.GetNewestBook();
+Console.WriteLine($"{newestBook.Title}");
+
+// Sum
+Console.WriteLine($"Total pages of books with 0 and 500 pages => " + queries.SumOfAllPageCount(0, 500));
+
+// Aggregate
+Console.WriteLine(queries.BooksBeforeYear(2015));
+
+// Average
+Console.WriteLine(queries.TitlesCharAverage());
+
+// GroupBy
+PrintGroups(queries.GetBooksBeforeYear(2000));
+
+// Lookup
+PrintDictionary(queries.BooksDictionary(), 'S');
+
+// Join
+PrintValues(queries.UsingJoinLinq());
